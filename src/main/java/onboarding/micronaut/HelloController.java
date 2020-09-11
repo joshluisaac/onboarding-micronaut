19056d054a283f8baa96b32f3bb5ad4ca484070f
Joshua Uzo <nwankwo.joshua@gmail.com>
2020-09-12 02:23:14 +1000

    Added HelloController

--- src/main/java/onboarding/micronaut/HelloController.java
+++ src/main/java/onboarding/micronaut/HelloController.java
@@ -1,17 +1,15 @@
 package onboarding.micronaut;
 
-import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
-
+import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Produces;
-import io.micronaut.runtime.Micronaut;
 
 @Controller("/hello")
 public class HelloController {
 
     @Get
-    @Produces
+    @Produces(MediaType.TEXT_PLAIN)
     public String index() {
         return "Hello World";
     }
